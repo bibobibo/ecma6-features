a028e22c1787f8ea4590dc938177d73365befb32
Dong Yuwei <yuwei.dong@rea-group.com>
2015-06-17 22:37:56 +0800

    map, set and template string

---
+++ map.js
@@ -0,0 +1,17 @@
+#! /usr/local/bin/node  --harmony
+
+'use strict';
+
+var m = new Map();
+var o = {
+    x: 'blah'
+};
+
+m.set(o, 'something to store');
+console.log(m.has(o)); // true
+console.log(m.get(o));
+
+//下面的代码在firefox中运行正常，在nodejs v0.12.4 中还有bug
+// for (let [k, v] of m){
+//     console.log(k, v);   
+// }
---
+++ template-string.js
@@ -0,0 +1,17 @@
+#! /usr/local/bin/iojs  --harmony
+
+'use strict';
+
+
+//下面的代码在firefox中运行正常，在nodejs v0.12.4 中还有bug, iojs v2.0.0 也正常。
+
+var name = 'Paul',  age = 99;
+
+var str = `$(name), is apparently $(age) years old`
+console.log(str)
+
+var longString = `here we have
+         a multiline string 
+         using backtick quotes`;
+
+console.log(longString)
