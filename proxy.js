71473dbc7c0ac4653d1c288394f2168b6542adad
Dong Yuwei <yuwei.dong@rea-group.com>
2015-06-25 22:06:27 +0800

    add proxy set(data binding) demo

--- proxy.js
+++ proxy.js
@@ -1,14 +1,25 @@
 //run in firefox 
 
-var target = {};
+var target = function(){}; //{}; //only Function is callable(apply/call);
 var handler = {
-  get: function (receiver, name) {
-    return `Hello, ${name}!`;
-  }
+    get: function(receiver, name) {
+        return `Hello, ${name}!`;
+    },
+    set: function(receiver, property, value) {
+        console.log("property ", property, " changed from", receiver[property], " to a new value: ", value)
+        receiver[property] = value;
+    },
+    apply: function(receiver, ...args) {
+        console.log('called with:', args);
+        return "I am the proxy!";
+    }
 };
 
-var p = new Proxy(target, handler);
-console.log(p.world);
+var model = new Proxy(target, handler);
+console.log(model.world);
+model.foo = "bad boy";
+model.foo = "good girl";
+model(123, 'abc');
 
 // var handler =
 // {
---
+++ str.js
@@ -0,0 +1,5 @@
+#! /usr/local/bin/node  --harmony 
+
+"foo bar".startsWith("foo");
+"foo bar".endsWith("bar");
+"foo bar".contains("foo");
